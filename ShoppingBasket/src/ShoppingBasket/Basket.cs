492b64d9387815ad039f1f65837775eb8093c446
ksterle <krunoslavsterle@gmail.com>
2020-01-17 20:26:10 +0100

    - initial implementation for Basket; BasketItem; ProductDiscount

---
+++ ShoppingBasket/src/ShoppingBasket/Basket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingBasket
+{
+    /// <summary>
+    /// Basket Facade.
+    /// </summary>
+    public class Basket
+    {
+        private ICollection<BasketItem> _basketItems;
+
+        public Basket()
+        {
+            _basketItems = new List<BasketItem>();
+        }
+
+        public Basket(ICollection<BasketItem> basketItems)
+        {
+            _basketItems = basketItems;
+        }
+
+        public IEnumerable<BasketItem> BasketItems => _basketItems;
+
+        public void AddToBasket(BasketItem basketItem)
+        {
+            var existing = _basketItems.FirstOrDefault(p => p.Id == basketItem.Id);
+            if (existing == null)
+                _basketItems.Add(basketItem);
+            else
+                existing.UpdateQuantity(existing.Quantity + 1);
+        }
+
+        public void RemoveFromBasket(Guid id)
+        {
+            var existing = _basketItems.FirstOrDefault(p => p.Id == id);
+            if (existing == null)
+                return;
+
+            if (existing.Quantity > 1)
+                existing.UpdateQuantity(existing.Quantity - 1);
+            else
+                _basketItems.Remove(existing);
+        }
+
+        public void GetTotals()
+        {
+            // TODO: IMPLEMENT THIS.
+        }
+    }
+}
---
+++ ShoppingBasket/src/ShoppingBasket/BasketItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingBasket
+{
+    /// <summary>
+    /// BasketItem implementation.
+    /// </summary>
+    public class BasketItem
+    {
+        private int _quantity = 0;
+
+        public BasketItem(Guid id, string name, decimal unitPrice, int? quantity)
+        {
+            Id = id;
+            Name = name;
+            UnitPrice = unitPrice;
+            _quantity = quantity ?? 1;
+            Discounts = new List<ProductDiscount>();
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public decimal UnitPrice { get; }
+        public int Quantity => _quantity;
+        
+        public ICollection<ProductDiscount> Discounts { get; }
+
+        public void UpdateQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+    }
+}
