292e7832401fb3168c70c12d22a494edf77a3d8a
ksterle <krunoslavsterle@gmail.com>
2020-01-17 20:02:04 +0100

    - initial unit tests

---
+++ ShoppingBasket/tests/UnitTests/ShoppingBasketTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace UnitTests
+{
+    public class ShoppingBasketTests
+    {
+        [Fact]
+        public void Given_BreadButterAndMilk_When_Total_Then_SumPrice()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Given_TwoButterTwoBread_When_Total_Than_IncludeDiscount()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Given_FourMilk_When_Total_Than_IncludeDiscount()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Given_TwoButterOneBreadEightMilk_When_Total_Than_IncludeDiscount()
+        {
+            Assert.True(false);
+        }
+    }
+}
